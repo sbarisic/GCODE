100757721f7c0820775c1eef20f67764490ee041
Saša Barišić <cartman300@net.hr>
2017-11-08 13:44:33 +0100

    Code

--- GCODE/GCODE.cs
+++ GCODE/GCODE.cs
@@ -4,7 +4,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using num = System.Single;
+
 namespace GCODE {
-	static class GCODE {
+	public static class GCODE {
+		static StringBuilder Out;
+
+		static GCODE() {
+			Out = new StringBuilder();
+		}
+
+		static string ToArg<T>(string VariableName, T Val) {
+			if (Val == null)
+				return "";
+
+			return VariableName + Val.ToString() + " ";
+		}
+
+		public static void Emit(string Src) {
+			Out.AppendLine(Src.Trim());
+		}
+
+		public static string GenerateGCode() {
+			Emit("M02");
+
+			return Out.ToString().Replace(',', '.');
+		}
+
+		// Other
+
+		public static void Feed(num Val) {
+			Emit("F" + Val);
+		}
+
+		public static void Comment(string Txt) {
+			Emit(string.Format(";({0})", Txt));
+		}
+
+		// G codes
+
+		public static void IMPERIAL() => Emit("G20");
+		public static void METRIC() => Emit("G21");
+
+		public static void Rapid(num? X = null, num? Y = null, num? Z = null) {
+			Emit("G0 " + ToArg("X", X) + ToArg("Y", Y) + ToArg("Z", Z));
+		}
+
+		public static void Linear(num? X = null, num? Y = null, num? Z = null) {
+			Emit("G1 " + ToArg("X", X) + ToArg("Y", Y) + ToArg("Z", Z));
+		}
+
+		// M codes
+
+		public static void SelectTool(int Num) {
+			Emit("M06 T" + Num);
+		}
+
+		public static void Spindle(int RPM, bool CCW = false) {
+			if (RPM == 0) {
+				Emit("M05");
+				return;
+			}
+
+			Emit("G97");
+			string Code = CCW ? "M04" : "M03";
+			Emit(Code + " S" + RPM);
+		}
 	}
 }
--- GCODE/Program.cs
+++ GCODE/Program.cs
@@ -3,12 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
+using static GCODE.GCODE;
 
 namespace GCODE {
 	class Program {
 		static void Main(string[] args) {
+			METRIC();
 
+			Feed(400);
+			SelectTool(1);
 
+			Rapid(Z: 1);
+			Rapid(X: 1, Y: 1);
+			Spindle(2000);
+
+			for (int i = 0; i < 2; i++) {
+				float Offset = 1.3f;
+				Comment("Start rectangle " + i);
+
+				Linear(X: 1 + i * Offset, Y: 1 + i * Offset);
+				Linear(Z: -1);
+				Linear(X: 9 - i * Offset);
+				Linear(Y: 9 - i * Offset);
+				Linear(X: 1 + i * Offset);
+				Linear(Y: 1 + i * Offset);
+				Linear(Z: 1);
+
+				Comment("Stop rectangle " + i);
+			}
+
+			Spindle(0);
+			Rapid(X: -1, Y: -1);
+
+			File.WriteAllText("test.gcode", GenerateGCode());
 		}
 	}
 }
