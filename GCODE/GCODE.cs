ad0afcd34cc8dba778270423d352d9f3ae9cf400
Saša Barišić <cartman300@net.hr>
2017-11-10 01:47:56 +0100

    More featurex

--- GCODE/GCODE.cs
+++ GCODE/GCODE.cs
@@ -3,72 +3,225 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
+using System.Globalization;
+using System.Drawing;
 
 using num = System.Single;
+using System.Drawing.Text;
+using System.Drawing.Drawing2D;
 
 namespace GCODE {
+	public class CodeGenerator {
+		StringBuilder Code;
+		int LineNum;
+		int LineInc;
+
+		public CodeGenerator() {
+			Code = new StringBuilder();
+			LineNum = 10;
+			LineInc = 10;
+		}
+
+		public void EmitLine(string Src) {
+			string F = string.Format("N{0} ", LineNum);
+
+			Console.Write(F);
+			Code.Append(F);
+			LineNum += LineInc;
+
+			Console.WriteLine(Src);
+			Code.AppendLine(Src);
+		}
+
+		public void EmitCode(string Code, params string[] Args) {
+			EmitLine(string.Format("{0} {1}", Code, string.Join(" ", Args)).Trim());
+		}
+
+		public void EmitCode(string Code, int Num, params string[] Args) {
+			EmitCode(string.Format("{0}{1}", Code, Num), Args);
+		}
+
+		public string Compile() {
+			return Code.ToString();
+		}
+	}
+
+	public enum Units {
+		Imperial = 1,
+		Metric = 2
+	}
+
 	public static class GCODE {
-		static StringBuilder Out;
+		static CodeGenerator Gen;
 
 		static GCODE() {
-			Out = new StringBuilder();
+			Gen = new CodeGenerator();
+			Units = Units.Metric;
+			Offset = new Vector3(0, 0, 0);
+			ToolDrawDepth = 2;
 		}
 
-		static string ToArg<T>(string VariableName, T Val) {
-			if (Val == null)
-				return "";
-
-			return VariableName + Val.ToString() + " ";
+		static string ToStr<T>(T Val) {
+			if (typeof(T) == typeof(num))
+				return ((num)(object)Val).ToString(CultureInfo.InvariantCulture);
+			return Val.ToString();
 		}
 
-		public static void Emit(string Src) {
-			Out.AppendLine(Src.Trim());
+		static string ToStr<T>(string Prefix, T Val) {
+			return Prefix + ToStr(Val);
 		}
 
-		public static string GenerateGCode() {
-			Emit("M02");
+		// PUBLIC
 
-			return Out.ToString().Replace(',', '.');
+		static Units _Units = (Units)0;
+		public static Units Units {
+			get {
+				return _Units;
+			}
+			set {
+				if (_Units != value) {
+					_Units = value;
+
+					if (_Units == Units.Imperial)
+						Gen.EmitCode("G", 20);
+					else if (_Units == Units.Metric) {
+						Gen.EmitCode("G", 21);
+					} else
+						throw new Exception("Invalid unit type " + _Units);
+				}
+			}
 		}
 
-		// Other
+		public static Vector3 Position {
+			get;
+			private set;
+		}
+
+		public static Vector3 Offset;
+		public static float ToolDrawDepth;
+
+		public static string Compile() {
+			return Gen.Compile();
+		}
 
 		public static void Feed(num Val) {
-			Emit("F" + Val);
+			Gen.EmitCode("F" + ToStr(Val));
 		}
 
 		public static void Comment(string Txt) {
-			Emit(string.Format(";({0})", Txt));
+			// TODO
+			//Emit(string.Format(";({0})", Txt));
 		}
 
 		// G codes
 
-		public static void IMPERIAL() => Emit("G20");
-		public static void METRIC() => Emit("G21");
-
-		public static void Rapid(num? X = null, num? Y = null, num? Z = null) {
-			Emit("G0 " + ToArg("X", X) + ToArg("Y", Y) + ToArg("Z", Z));
+		public static void Rapid(Vector3 Pos) {
+			Position = Pos += Offset;
+			Gen.EmitCode("G", 0, ToStr("X", Pos.X), ToStr("Y", Pos.Y), ToStr("Z", Pos.Z));
 		}
 
-		public static void Linear(num? X = null, num? Y = null, num? Z = null) {
-			Emit("G1 " + ToArg("X", X) + ToArg("Y", Y) + ToArg("Z", Z));
+		public static void Rapid(float X, float Y, float Z) {
+			Rapid(new Vector3(X, Y, Z));
+		}
+
+		public static void Linear(Vector3 Pos) {
+			Position = Pos += Offset;
+			Gen.EmitCode("G", 1, ToStr("X", Pos.X), ToStr("Y", Pos.Y), ToStr("Z", Pos.Z));
+		}
+
+		public static void Linear(float X, float Y, float Z) {
+			Linear(new Vector3(X, Y, Z));
+		}
+
+		public static void Rectangle(float W, float H) {
+			Vector3 S = Position;
+			Vector3 OldOffset = Offset;
+			Offset = Vector3.Zero;
+			Linear(S + new Vector3(W, 0, 0));
+			Linear(S + new Vector3(W, H, 0));
+			Linear(S + new Vector3(0, H, 0));
+			Linear(S);
+			Offset = OldOffset;
+		}
+
+		public static RectangleF Path(GraphicsPath P) {
+			Vector2 Scale = new Vector2(1, -1);
+
+			PointF[] Points = P.PathData.Points;
+			byte[] Types = P.PathData.Types;
+
+			int LastFirst = 0;
+			int i = 0;
+
+			Vector3 Start = Position;
+			Vector3 OldOffset = Offset;
+			Offset = Start + Offset;
+
+			for (i = 0; i < Points.Length; i++) {
+				byte PointType = (byte)(Types[i] & 0x7);
+				bool FirstPoint = Types[i] == 0;
+				bool LastPoint = (Types[i] & 0x80) != 0;
+
+				if (FirstPoint)
+					LastFirst = i;
+
+				float X = Points[i].X * Scale.X;
+				float Y = Points[i].Y * Scale.Y;
+
+				if (FirstPoint)
+					Rapid(X, Y, 0);
+
+				Linear(X, Y, -ToolDrawDepth);
+
+				if (LastPoint) {
+					X = Points[LastFirst].X * Scale.X;
+					Y = Points[LastFirst].Y * Scale.Y;
+
+					Linear(X, Y, -ToolDrawDepth);
+					Linear(X, Y, 0);
+				}
+			}
+
+			Offset = OldOffset;
+
+			RectangleF R = P.GetBounds();
+			R.Y = -R.Y;
+			R.Height = -R.Height;
+			return R;
+		}
+
+		public static RectangleF PathText(string Txt, string FontFile, float FontSize, StringAlignment H = StringAlignment.Near, StringAlignment V = StringAlignment.Far) {
+			PrivateFontCollection Collection = new PrivateFontCollection();
+			Collection.AddFontFile(FontFile);
+			Font F = new Font(Collection.Families[0], FontSize);
+
+			StringFormat SF = new StringFormat();
+			SF.Alignment = H;
+			SF.LineAlignment = V;
+
+			GraphicsPath P = new GraphicsPath();
+			P.AddString(Txt, F.FontFamily, (int)F.Style, F.Size, new PointF(0, 0), SF);
+			P.Flatten();
+
+			return Path(P);
 		}
 
 		// M codes
 
 		public static void SelectTool(int Num) {
-			Emit("M06 T" + Num);
+			Gen.EmitCode("M", 6, ToStr("T", Num));
 		}
 
 		public static void Spindle(int RPM, bool CCW = false) {
 			if (RPM == 0) {
-				Emit("M05");
+				Gen.EmitCode("M", 5);
 				return;
 			}
 
-			Emit("G97");
-			string Code = CCW ? "M04" : "M03";
-			Emit(Code + " S" + RPM);
+			Gen.EmitCode("G", 97);
+			int Code = CCW ? 4 : 3;
+			Gen.EmitCode("M", Code, ToStr("S", RPM));
 		}
 	}
 }
--- GCODE/Program.cs
+++ GCODE/Program.cs
@@ -4,40 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Numerics;
+using System.Drawing.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using static GCODE.GCODE;
 
 namespace GCODE {
 	class Program {
 		static void Main(string[] args) {
-			METRIC();
-
 			Feed(400);
+			Rapid(0, 0, 1);
 			SelectTool(1);
-
-			Rapid(Z: 1);
-			Rapid(X: 1, Y: 1);
 			Spindle(2000);
 
-			for (int i = 0; i < 2; i++) {
-				float Offset = 1.3f;
-				Comment("Start rectangle " + i);
+			ToolDrawDepth = 1.2f;
+			RectangleF R = PathText("Hello", "fonts\\1CamBam_Stick_1.ttf", 10.0f);
 
-				Linear(X: 1 + i * Offset, Y: 1 + i * Offset);
-				Linear(Z: -1);
-				Linear(X: 9 - i * Offset);
-				Linear(Y: 9 - i * Offset);
-				Linear(X: 1 + i * Offset);
-				Linear(Y: 1 + i * Offset);
-				Linear(Z: 1);
+			SelectTool(2);
+			R.Inflate(1, -1);
 
-				Comment("Stop rectangle " + i);
+			for (int i = 0; i < 4; i++) {
+				R.Inflate(0.9f, -0.8f);
+				Rapid(R.X, R.Y, 1);
+				Linear(R.X, R.Y, -1f);
+				Rectangle(R.Width, R.Height);
+				Linear(R.X, R.Y, 1);
 			}
 
-			Spindle(0);
-			Rapid(X: -1, Y: -1);
 
-			File.WriteAllText("test.gcode", GenerateGCode());
+			Spindle(0);
+			Offset = Vector3.Zero;
+			Rapid(new Vector3(0, 0, 1));
+
+			File.WriteAllText("test.gcode", Compile());
 		}
 	}
 }
